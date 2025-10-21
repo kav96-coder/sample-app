05da8a274eab963545cbbf1003507c774a084e28
dineshpardhu-210 <dineshpardhu@gmail.com>
2025-10-21 10:09:31 +0000

    Saved local changes before pull

--- app/index.js
+++ app/index.js
@@ -1,8 +1,7 @@
+// Simple Node test app
 const http = require('http');
 const PORT = 3000;
-
 const server = http.createServer((req, res) => {
-  res.end(`Hey there, your sample-app is up and running. Time: ${new Date()}\n`);
+  res.end(`Hey there, your sample-app is up and running Time: ${new Date()}\n`);
 });
-
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
