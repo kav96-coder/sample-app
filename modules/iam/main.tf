cdea975290f1a8385ca63f28a69cbc182829ee69
Sandeep Sheshagiri <mailinsandeeps@gmail.com>
2025-10-23 14:59:34 +0530

    iam

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -21,19 +21,35 @@ resource "aws_iam_role" "this" {
   })
 }
 
-# Attach necessary policies
+#############################################
+# Attach necessary policies to Jenkins EC2 role
+#############################################
+
 resource "aws_iam_role_policy_attachment" "jenkins_policies" {
   for_each = toset([
+    # Existing policies
     "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore",
     "arn:aws:iam::aws:policy/AmazonEC2ReadOnlyAccess",
-    "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
+    "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess",
+
+    # EKS & node access policies
+    "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy",
+    "arn:aws:iam::aws:policy/AmazonEKSServicePolicy",
+    "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy",
+    "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy",
+
+    # Docker image access (if using ECR)
+    "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
   ])
 
   role       = aws_iam_role.this.name
   policy_arn = each.value
 }
 
+#############################################
 # Instance Profile for EC2
+#############################################
+
 resource "aws_iam_instance_profile" "this" {
   name = "${var.name}-instance-profile"
   role = aws_iam_role.this.name
